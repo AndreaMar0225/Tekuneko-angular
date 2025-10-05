2c70b46909a7b6f23e48f4a78e4725ffebc8e1c0
Andrea Ortiz <maraortiz0225@gmail.com>
2025-10-05 14:20:00 -0500

    Creacion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './paginas/home/home';
+import { Nosotros } from './paginas/nosotros/nosotros';
+import { Servicios } from './paginas/servicios/servicios';
+import { InicioSesion } from './paginas/inicio-sesion/inicio-sesion';
+import { GestionarServicios } from './paginas/gestionar-servicios/gestionar-servicios';
+import { CrearServicio } from './paginas/crear-servicio/crear-servicio';
+import { EditarServicio } from './paginas/editar-servicio/editar-servicio';
+import { DetalleServicio } from './paginas/detalle-servicio/detalle-servicio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component: Home},
+    {path:'Nosotros', component: Nosotros},
+    {path:'Servicios', component: Servicios},
+    {path:'InicioSesion', component: InicioSesion},
+    {path:'GestionarServicios', component: GestionarServicios},
+    {path:'CrearServicio', component: CrearServicio},
+    {path:'EditarServicio', component: EditarServicio},
+    {path:'DetalleServicio', component: DetalleServicio},    
+    {path:'**',redirectTo:''},
+];
